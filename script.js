6f0bf67855084246ff16fe4f83965b7cbe8b963e
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2025-02-16 11:38:36 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:3000/songs/");
   let response = await a.text();
-  console.log(response);
+//   console.log(response);
   let div = document.createElement("div");
   div.innerHTML = response;
   let as = div.getElementsByTagName("a");
@@ -14,6 +14,36 @@ async function getSongs() {
       songs.push(element.href);
     }
   }
-  console.log(songs);
+  return songs;
 }
-getSongs();
+
+async function main() {
+    // get songs list
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songUl = document.querySelector(".songList").getElementsByTagName("li")[0]
+
+  for (const song of songs) {
+    songUl.innerHTML = songUl.innerHTML + song;
+  }
+
+//   play audio 
+var audio = new Audio(songs[0]);
+// audio.play();
+
+
+const audioElement = new Audio(songs[0]);
+audioElement.addEventListener("loadeddata", () => {
+  let duration = audioElement.duration;
+  // The duration variable now holds the duration (in seconds) of the audio clip
+  
+  setInterval(()=>{
+      console.log(audio.duration, audio.currentSrc, audio.currentTime)
+
+  },1000)
+});
+
+}
+
+main();
