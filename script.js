23fa21a78bbc66eb458a1f491500cdc57c6d1fda
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2025-02-24 14:21:52 +0530

    update

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ document.querySelector(".hamburger").addEventListener("click", ()=>{
   document.querySelector(".left").style.left = "0"
 })
 document.querySelector(".close").addEventListener("click", ()=>{
-  document.querySelector(".left").style.left = "-100%"
+  document.querySelector(".left").style.left = "-120%"
 })
 
 }
