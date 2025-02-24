dffa78acf2208f8e81bc103082b864d7f12a4639
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2025-02-24 13:46:18 +0530

    update

--- script.js
+++ script.js
@@ -112,7 +112,13 @@ async function main() {
     document.querySelector(".circle").style.left = percent + "%" ;
     curruntSong.currentTime = ((curruntSong.duration) * percent)/100
   } )
-
+// add an event listenr for hamburger
+document.querySelector(".hamburger").addEventListener("click", ()=>{
+  document.querySelector(".left").style.left = "0"
+})
+document.querySelector(".close").addEventListener("click", ()=>{
+  document.querySelector(".left").style.left = "-100%"
+})
 
 }
 
