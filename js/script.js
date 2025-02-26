eeb4a3d98467e56eca1f3a1909d374441879809c
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2025-02-26 13:44:34 +0530

    js ke line 105 me && !e.href.includes("htaccess")) add kiya

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ async function diplayAlbums() {
   for (let index = 0; index < array.length; index++) {
     const e = array[index];
 
-    if (e.href.includes("/songs")) {
+    if (e.href.includes("/songs") && !e.href.includes("htaccess")) {
       let folder = e.href.split("/").slice(-2)[0];
 
       // get the metadata of the folder
